5d63c03b1e8deed82397f72c480567e4799c046f
Habiba <habiba.abdeldayem.dev@gmail.com>
2025-09-01 11:04:27 +0300

    first commit

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:quotes/features/random_quote/presentation/screens/quote_screen.dart';
+
+class QuoteApp extends StatelessWidget {
+  const QuoteApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: QuoteScreen(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+    );
+  }
+}

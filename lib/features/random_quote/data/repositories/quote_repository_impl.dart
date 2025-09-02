d57bfa30ccfa1f565e2065a0c2503b011a54c591
Habiba <habiba.abdeldayem.dev@gmail.com>
2025-09-02 16:43:55 +0300

    feat(data): finish data source contracts and implement QuoteRepository - Define remote and local data source contracts - Complete QuoteRepository implementation with error handling

--- lib/features/random_quote/data/repositories/quote_repository_impl.dart
+++ lib/features/random_quote/data/repositories/quote_repository_impl.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:quotes/core/error/exceptions.dart';
 import 'package:quotes/core/error/failures.dart';
 import 'package:quotes/core/network/network_info.dart';
 import 'package:quotes/features/random_quote/data/datasources/random_quote_local_data_source.dart';
@@ -14,9 +15,24 @@ class QuoteRepositoryImpl implements QuoteRepository{
   QuoteRepositoryImpl({required this.networkInfo, required this.remoteDataSource, required this.localDataSource});
   
   @override
-  Future<Either<Failure, Quote>> getRandomQuote() {
-    // TODO: implement getRandomQuote
-    throw UnimplementedError();
+  Future<Either<Failure, Quote>> getRandomQuote() async{
+    if(await networkInfo.isConnected){
+      try{
+        final remoteRandomQuote = await remoteDataSource.getRandomQuote();
+        localDataSource.cacheQuote(remoteRandomQuote);
+        return Right(remoteRandomQuote);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+try{
+        final cacheRandomQuote = await localDataSource.getLastRandomQuote();
+        localDataSource.cacheQuote(cacheRandomQuote);
+        return Right(cacheRandomQuote);
+      } on CacheException {
+        return Left(CacheFailure());
+      }
+    }  
   }
 
 

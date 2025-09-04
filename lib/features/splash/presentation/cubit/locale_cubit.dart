2f1cc6a7a87c4a17b113d1a5f496df110fedc390
Habiba <habiba.abdeldayem.dev@gmail.com>
2025-09-04 19:08:00 +0300

    feat: added splash screen

---
+++ lib/features/splash/presentation/cubit/locale_cubit.dart
@@ -0,0 +1,9 @@
+
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'locale_state.dart';
+
+class LocaleCubit extends Cubit<LocaleState> {
+  LocaleCubit() : super(LocaleInitial());
+}
---
+++ lib/features/splash/presentation/cubit/locale_state.dart
@@ -0,0 +1,10 @@
+part of 'locale_cubit.dart';
+
+sealed class LocaleState extends Equatable {
+  const LocaleState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class LocaleInitial extends LocaleState {}
